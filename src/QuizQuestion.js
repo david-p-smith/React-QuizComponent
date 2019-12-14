b2e55f370eea61952528cf6148f1152605ffee16
DPS <smith.dp@gmail.com>
2019-12-14 15:08:32 +0000

    structure for qq html skeleton

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,7 +6,16 @@ class QuizQuestion extends Component {
     render() {
         return (
             <div>
-
+                 <main>
+                    <section>
+                    <p>// instruction text goes here</p>
+                    </section>
+                    <section className="buttons">
+                    <ul>
+                        //quiz question button logic goes here
+                    </ul>
+                    </section>
+                </main>
             </div>
         )
     }
