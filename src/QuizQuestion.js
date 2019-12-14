289d2c0838d89b626de7dd92f1e993dac623797c
DPS <smith.dp@gmail.com>
2019-12-14 15:08:13 +0000

    quiz question skeleton

---
+++ src/QuizQuestion.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import React, { Component } from 'react'
+
+class QuizQuestion extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default QuizQuestion;
